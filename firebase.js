7bb151a9f5bcf1c83be877d1f0caa4d351c7248b
Timix249 <timurschule4@gmail.com>
2026-03-15 20:37:37 +0100

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDhGbebbzcZjztxL-8NFfDTXPSK8o5HstI",
+  authDomain: "points-system-bed03.firebaseapp.com",
+  projectId: "points-system-bed03",
+  storageBucket: "points-system-bed03.firebasestorage.app",
+  messagingSenderId: "633156992686",
+  appId: "1:633156992686:web:6d29209dcc2b01bcf1b73f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
