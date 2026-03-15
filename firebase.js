d1a779dc0ced148edcb95ba04d60909fee1aeae5
Timix249 <timurschule4@gmail.com>
2026-03-15 20:54:54 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,9 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore, doc, setDoc, updateDoc, getDoc, onSnapshot, increment, collection } 
+from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDhGbebbzcZjztxL-8NFfDTXPSK8o5HstI",
+  apiKey: "AIzaSyDhGbeBBzcZjztxL-8NFDfIXPSK8o5HstI",
   authDomain: "points-system-bed03.firebaseapp.com",
   projectId: "points-system-bed03",
   storageBucket: "points-system-bed03.firebasestorage.app",
@@ -7,5 +11,14 @@ const firebaseConfig = {
   appId: "1:633156992686:web:6d29209dcc2b01bcf1b73f"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+window.db = db;
+window.doc = doc;
+window.setDoc = setDoc;
+window.updateDoc = updateDoc;
+window.getDoc = getDoc;
+window.onSnapshot = onSnapshot;
+window.increment = increment;
+window.collection = collection;
